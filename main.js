0df6106ff714ebec33a4f705b657ca6b7a44a302
Abigail <abigail@dogbuilt.net>
2022-06-21 23:12:16 -0400

    Add demiboy and vincian flags

--- main.js
+++ main.js
@@ -9,29 +9,39 @@
 */
 const validFlags = [
   {
-    name: "lesbian",
+    name: "lesbian", // lesbian
     colors: ["#D62900", "#F07722", "#FF9B55", "#FFFFFF", "#D262A6", "#B75591", "#A50062"],
     stripeWidth: 1/7
   },
   {
-    name: "enby",
+    name: "enby", // y
     colors: ["#FCF431", "#FCFCFC", "#9D59D2", "#282828"],
     stripeWidth: 1/4
   },
   {
-    name: "pan",
+    name: "pan", // p
     colors: ["#FF1C8D", "#FFD700", "#1AB3FF"],
     stripeWidth: 1/3
   },
   {
-    name: "fluid", // genderfluid, not gender solid
+    name: "fluid", // fud, genderfluid, not gender solid
     colors: ["#FE76A2", "#FFFFFF", "#BF12D7", "#000000", "#303CBE"],
     stripeWidth: 1/5
   },
   {
-    name: "tra", // trans, only using t/r/a because of repeated color usage
+    name: "tra", // tr, trans, only using t/r/a because of repeated color usage
     colors: ["#5BCFFB", "#F5ABB9", "#FFFFFF", "#F5ABB9", "#5BCFFB"],
     stripeWidth: 1/5
+  },
+  {
+    name: "demiboy", // mo
+    colors: ["#808080", "#C4C4C4", "#9AD9EB", "#FFFFFF", "#9AD9EB", "#C4C4C4", "#808080"],
+    stripeWidth: 1/7
+  },
+  {
+    name: "v", // v, vincian, not doing c because it's another white
+    colors: ["#078D70", "#26CEAA", "#98E8C1", "#FFFFFF", "#7BADE2", "#5049CC", "#3D1A78"],
+    stripeWidth: 1/7
   }
 ];
 
