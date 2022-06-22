a31028d0169792d3bab27deaa58306978573f3a1
Abigail <abigail@dogbuilt.net>
2022-06-22 16:55:22 -0400

    Fix formatting in serve.js

--- serve.js
+++ serve.js
@@ -62,7 +62,7 @@ const requestListener = function (req, res) {
     }
 
     if (input.length > 15) {
-      res.writeHead(406, { "Content-Type": "text/text"});
+      res.writeHead(406, { "Content-Type": "text/text" });
       res.end("Input too long! Should be under 15 characters.");
       return;
     }
