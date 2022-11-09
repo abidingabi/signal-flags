36cf42885a399e947f07501f9fed5a9e513472e7
Abigail <abigail@dogbuilt.net>
2022-11-08 22:17:34 -0500

    Remove nodejs server
    Also merges the javascript code for the client back into one file for simplicity

--- static/main.js
+++ static/main.js
@@ -1,26 +1,133 @@
-import * as lib from "/lib.js";
+"use strict";
+
+/* just so we're clear, all flags are valid;
+ * these flags are specifically valid here because
+ * they have a one-to-one correspondence between a color and a descriptor of the flag,
+ * or can be truncated to be that (e.g. the tra in trans)
+ *
+ * sorted in priority order; e.g. the n in lesbian takes priority over the n in enby
+ */
+const validFlags = [
+  {
+    name: "lesbian", // lesbian
+    colors: [
+      "#D62900",
+      "#F07722",
+      "#FF9B55",
+      "#FFFFFF",
+      "#D262A6",
+      "#B75591",
+      "#A50062",
+    ],
+    stripeWidth: 1 / 7,
+  },
+  {
+    name: "enby", // y
+    colors: ["#FCF431", "#FCFCFC", "#9D59D2", "#282828"],
+    stripeWidth: 1 / 4,
+  },
+  {
+    name: "pan", // p
+    colors: ["#FF1C8D", "#FFD700", "#1AB3FF"],
+    stripeWidth: 1 / 3,
+  },
+  {
+    name: "fluid", // fud, genderfluid, not gender solid
+    colors: ["#FE76A2", "#FFFFFF", "#BF12D7", "#000000", "#303CBE"],
+    stripeWidth: 1 / 5,
+  },
+  {
+    name: "tra", // tr, trans, only using t/r/a because of repeated color usage
+    colors: ["#5BCFFB", "#F5ABB9", "#FFFFFF", "#F5ABB9", "#5BCFFB"],
+    stripeWidth: 1 / 5,
+  },
+  {
+    name: "demiboy", // mo
+    colors: [
+      "#808080",
+      "#C4C4C4",
+      "#9AD9EB",
+      "#FFFFFF",
+      "#9AD9EB",
+      "#C4C4C4",
+      "#808080",
+    ],
+    stripeWidth: 1 / 7,
+  },
+  {
+    name: "cupio", // c
+    colors: ["#FCA9A3", "#FDC5C0", "#FFFFFF", "#C8BFE6", "#A0A0A0"],
+    stripeWidth: 1 / 5,
+  },
+  {
+    name: "vincian", // v
+    colors: [
+      "#078D70",
+      "#26CEAA",
+      "#98E8C1",
+      "#FFFFFF",
+      "#7BADE2",
+      "#5049CC",
+      "#3D1A78",
+    ],
+    stripeWidth: 1 / 7,
+  },
+];
+
+const uniqueLetters = validFlags
+  .map((flag) => flag.name.split(""))
+  .flat()
+  .filter(
+    (value, index, list) => list.indexOf(value) === index
+    // checks for if it is the first instance
+  )
+  .sort();
+
+const validationRegex =
+  "[" +
+  uniqueLetters.join("") +
+  uniqueLetters.map((c) => c.toUpperCase()).join("") +
+  "]+";
+
+function inputStringToColors(input) {
+  return input.split("").map((letter) => {
+    const flag = validFlags.find((flag) => flag.name.includes(letter));
+    return {
+      color: flag.colors[flag.name.indexOf(letter)],
+      stripeWidth: flag.stripeWidth,
+    };
+  });
+}
 
 const FLAG_WIDTH = 1500;
 const NORMAL_FLAG_HEIGHT = 1000;
 
-function drawFlag(canvas, ctx, rectsAndHeight) {
+function drawFlag(canvas, ctx, colors) {
   canvas.width = FLAG_WIDTH;
-  canvas.height = rectsAndHeight.height;
 
-  const rects = rectsAndHeight.rects;
+  // calculates the total flag height
+  var totalHeight = 0;
+  for (var i = 0; i < colors.length; i++) {
+    totalHeight += colors[i].stripeWidth * NORMAL_FLAG_HEIGHT - 1;
+    // 1 is subtracted so they line up nicely
+  }
+  canvas.height = totalHeight;
 
-  for (var i = 0; i < rects.length; i++) {
-    const rect = rects[i];
+  var currentPosition = 0;
 
-    ctx.fillStyle = rect.color;
-    ctx.fillRect(rect.x, rect.y, rect.width, rect.height);
+  for (var i = 0; i < colors.length; i++) {
+    const stripeWidth = colors[i].stripeWidth * NORMAL_FLAG_HEIGHT;
+
+    ctx.fillStyle = colors[i].color;
+    ctx.fillRect(0, currentPosition, FLAG_WIDTH, stripeWidth);
+
+    currentPosition += stripeWidth - 1;
   }
 }
 
 window.onload = function () {
-  document.getElementById(
-    "allowed-letters"
-  ).innerHTML += lib.uniqueLetters.join(", ");
+  document.getElementById("allowed-letters").innerHTML +=
+    uniqueLetters.join(", ");
 
   const startingURL = new URL(window.location.href);
   const searchParams = new URLSearchParams(startingURL.searchParams);
@@ -30,7 +137,7 @@ window.onload = function () {
   }
 
   const input = document.getElementById("desired-words");
-  input.pattern = lib.validationRegex;
+  input.pattern = validationRegex;
   input.value = searchParams.get("input");
 
   input.oninput = function () {
@@ -42,15 +149,7 @@ window.onload = function () {
     const ctx = canvas.getContext("2d");
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawFlag(
-      canvas,
-      ctx,
-      lib.calculateRectsAndHeight(
-        lib.inputStringToColors(input.value.toLowerCase()),
-        FLAG_WIDTH,
-        NORMAL_FLAG_HEIGHT
-      )
-    );
+    drawFlag(canvas, ctx, inputStringToColors(input.value.toLowerCase()));
   };
 
   input.onchange = function () {
